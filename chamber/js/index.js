8f15075884af2ef876ad8600c4e03b447dba4736
xkevd <kevinalex11@hotmail.com>
2022-06-11 16:25:30 -0300

    Discover finished

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -37,3 +37,16 @@ document.getElementById("modification").textContent = "Last modification: "+modD
 
 /* --- Discover --- */
 
+const numberVisits = document.querySelector("#numberVisits");
+const lastVisit = document.querySelector("#lastVisit");
+
+let visits = Number(window.localStorage.getItem("visit-ls"));
+if (visits !== 0){
+    numberVisits.textContent = visits;
+}
+else{
+    numberVisits.textContent = "This is your first visit"
+}
+
+visits++;
+localStorage.setItem("visit-ls", visits);
