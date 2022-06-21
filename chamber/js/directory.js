ed8519e3267e02f93320940fc5a6152e1f9ed42b
xkevd <kevinalex11@hotmail.com>
2022-06-21 19:09:54 -0300

    Cont...

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -16,15 +16,19 @@ function displayWebsite(website) {
     let card = document.createElement('section');
     card.id = "website-card";
     let h2 = document.createElement('h2');
+    h2.id = "website-title";
     let portrait = document.createElement('img');
 
     h2.textContent = `${website.name}`
-  
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    
+    // Site logo //
     portrait.setAttribute('src', website.picture);
     portrait.classList.add("website-logo");
     portrait.setAttribute('alt', `${website.name}`);
     portrait.setAttribute('loading', 'lazy');
+    portrait.id = "website-logo";
+
+    // Membership levels //
     let memberContainer = document.createElement("div");
     memberContainer.id = "member-container";
     let siteLink = document.createElement("a");
@@ -32,6 +36,7 @@ function displayWebsite(website) {
     let level = document.createElement("p");
     level.id = "website-level";
     let levelImg = document.createElement("img");
+    levelImg.id = "level-img";
     if(website.level == "Gold"){
         levelImg.setAttribute("src", "images/directory/gold.png")
         levelImg.classList.add("gold")
@@ -42,15 +47,19 @@ function displayWebsite(website) {
         levelImg.setAttribute("src", "images/directory/bronze.png")
         levelImg.classList.add("bronze")
     }
+
+    // Other data //
     let address = document.createElement("p");
+    address.id = "website-address";
     let phone = document.createElement("p");
+    phone.id = "website-phone";
 
     address.innerHTML = `<strong>Address:</strong> ${website.address}`;
     phone.innerHTML = `<strong>Phone: </strong>${website.phone}`;
     siteLink.innerHTML = `<strong>${website.website}</strong>`
     level.innerHTML = `<strong>${website.level} members</strong>`
 
-    // Add/append the section(card) with the h2 element
+    // Add elements
     card.appendChild(h2);
     card.appendChild(memberContainer);
     memberContainer.appendChild(levelImg);
