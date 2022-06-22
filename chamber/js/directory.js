f453093c83fd89487f0879ec063da379c1767ddf
xkevd <kevinalex11@hotmail.com>
2022-06-22 00:02:10 -0300

    Directory finished

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -11,32 +11,28 @@ fetch(requestURL)
     websites.forEach(displayWebsite);
   });
 
-
 function displayWebsite(website) {
     let card = document.createElement('section');
-    card.id = "website-card";
+    card.className += "website-card";
+    // Title //
     let h2 = document.createElement('h2');
-    h2.id = "website-title";
+    h2.className += "website-title";
     let portrait = document.createElement('img');
-
     h2.textContent = `${website.name}`
-    
     // Site logo //
     portrait.setAttribute('src', website.picture);
     portrait.classList.add("website-logo");
     portrait.setAttribute('alt', `${website.name}`);
     portrait.setAttribute('loading', 'lazy');
-    portrait.id = "website-logo";
-
     // Membership levels //
     let memberContainer = document.createElement("div");
-    memberContainer.id = "member-container";
+    memberContainer.className += "member-container";
     let siteLink = document.createElement("a");
     siteLink.setAttribute('href', `${website.website}`)
+    siteLink.className += "website-link";
     let level = document.createElement("p");
-    level.id = "website-level";
+    level.className += "website-level";
     let levelImg = document.createElement("img");
-    levelImg.id = "level-img";
     if(website.level == "Gold"){
         levelImg.setAttribute("src", "images/directory/gold.png")
         levelImg.classList.add("gold")
@@ -47,29 +43,58 @@ function displayWebsite(website) {
         levelImg.setAttribute("src", "images/directory/bronze.png")
         levelImg.classList.add("bronze")
     }
-
     // Other data //
     let address = document.createElement("p");
-    address.id = "website-address";
+    address.className += "website-address";
     let phone = document.createElement("p");
-    phone.id = "website-phone";
+    phone.className += "website-phone";
 
     address.innerHTML = `<strong>Address:</strong> ${website.address}`;
     phone.innerHTML = `<strong>Phone: </strong>${website.phone}`;
     siteLink.innerHTML = `<strong>${website.website}</strong>`
     level.innerHTML = `<strong>${website.level} members</strong>`
-
     // Add elements
-    card.appendChild(h2);
-    card.appendChild(memberContainer);
-    memberContainer.appendChild(levelImg);
-    memberContainer.appendChild(level);
-    card.appendChild(portrait);
-    card.appendChild(siteLink)
-    card.appendChild(address);
-    card.appendChild(phone);
-
-  
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('#directory-body').appendChild(card);
+        if(cards.className == "grid-display"){
+            card.appendChild(h2);
+            card.appendChild(memberContainer);
+            memberContainer.appendChild(levelImg);
+            memberContainer.appendChild(level);
+            card.appendChild(portrait);
+            card.appendChild(siteLink)
+            card.appendChild(address);
+            card.appendChild(phone);
+            // Add/append the existing HTML div with the cards class with the section(card)
+            document.querySelector('#directory-body').appendChild(card);
+        }else{
+            card.appendChild(h2);
+            card.appendChild(levelImg);
+            card.appendChild(siteLink)
+            // Add/append the existing HTML div with the cards class with the section(card)
+            document.querySelector('#directory-body').appendChild(card);
+        }
+        const gridButton = document.querySelector("#set-grid");
+        const listButton = document.querySelector("#set-list");
+      
+        gridButton.addEventListener("click", ()=>{
+            card.appendChild(h2);
+            card.appendChild(memberContainer);
+            memberContainer.appendChild(levelImg);
+            memberContainer.appendChild(level);
+            card.appendChild(portrait);
+            card.appendChild(siteLink)
+            card.appendChild(address);
+            card.appendChild(phone);
+            cards.classList.remove("list-display");
+            cards.classList.add("grid-display");
+        })
+        listButton.addEventListener("click", ()=>{
+            memberContainer.removeChild(level);
+            card.removeChild(portrait);
+            card.removeChild(address);
+            card.removeChild(phone);
+            cards.classList.add("list-display");
+            cards.classList.remove("grid-display");
+        })
   }
+
+
