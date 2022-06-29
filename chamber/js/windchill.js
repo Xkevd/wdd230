c47dea9d4534c020b8eec8e15cc32c864935ec97
xkevd <kevinalex11@hotmail.com>
2022-06-28 23:20:24 -0300

    Weather fixed with api

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,19 +1,54 @@
-/*Temperatures*/
-const temperature = parseInt(document.getElementById("temperature").textContent);
-const fahrenheit = (temperature * 1.8) + 32;
-/*Wind*/
-const windSpeed = parseFloat(document.getElementById("speed").textContent);
-const speedMph = windSpeed * 0.609344;
+
+const temperature = document.getElementById("temperature");
+const windSpeed = document.getElementById("speed");
 const windChillElement = document.getElementById("chill");
+//Working with the API
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Montevideo&appid=66ebc759ac525615852406cc5e9884de&units=metric';
+const weatherIcon = document.querySelector('#temp-icon');
+const captionDesc = document.querySelector('#weather figcaption');
 
-
-const windChill = (fahrenheit, speedMph) =>{
-   return (35.74 + (0.6215 * fahrenheit) - (35.75 * (speedMph ** 0.16)) + (0.4275 * (fahrenheit * (speedMph ** 0.16))))};
-
-
-if (fahrenheit <= 50 && speedMph > 3.0) {
-  let windChillCel = (windChill(fahrenheit, speedMph)-32)*0.6;
-  windChillElement.textContent = parseInt(windChillCel) + "°C";
-} else {
-  windChillElement.textContent = "N/A";
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
 }
+
+let wordsArray
+function capitalize(words){
+    let wordsArray = words.split(" ");
+    for (let i = 0; i < wordsArray.length; i++) {
+        wordsArray[i] = `${wordsArray[i].charAt(0).toUpperCase()}${wordsArray[i].slice(1).toLowerCase()}`;
+  }
+  return wordsArray.join(" ");
+}
+function displayResults(weatherData) {
+  temperature.textContent = `${weatherData.main.temp.toFixed(0)}`;
+  windSpeed.innerHTML = `${weatherData.wind.speed.toFixed(0)}`;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = capitalize(desc);
+  
+  const windChill = (temperature, windSpeed)=>{
+    return (13.12 + (0.6215*temperature) - (11.37*(windSpeed**0.16)) + (0.3965*(temperature*(windSpeed**0.16))))};
+  let currentTemp = weatherData.main.temp;
+  let currentWind = weatherData.wind.speed;
+  if (currentTemp <= 10 && currentWind > 4.8) {
+    windChillElement.textContent = `${windChill(currentTemp, currentWind).toFixed(0)}°C`;
+  } else {
+   windChillElement.textContent = "N/A";
+  }
+}
+apiFetch();
+
+//Windchill and data processing
