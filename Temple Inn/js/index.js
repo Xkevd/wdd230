9dce1c1387b994d7084ccb3412cbd8ef9a9ccbe8
xkevd <kevinalex11@hotmail.com>
2022-07-15 21:36:23 -0300

    changes

--- Temple Inn/js/index.js
+++ Temple Inn/js/index.js
@@ -2,8 +2,6 @@
 
 function toggleMenu(){
     document.querySelector("#menuList").classList.toggle("open");
-    //document.querySelector("#facebook").classList.toggle("open");
-    //document.querySelector("#logo").classList.toggle("open");
     document.querySelector("#weather-div").classList.toggle("open");
     document.querySelector("#menuButton").classList.toggle("open");
 }
